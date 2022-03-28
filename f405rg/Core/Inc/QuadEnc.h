4a458527b25f5a31ae51a5950aadcea7b9f00d07
Tyler Tucker <tylermatthewtucker@gmail.com>
2022-03-28 15:21:14 -0500

    Added more functions and removed DigitalIn stuff

--- f405rg/Core/Inc/QuadEnc.h
+++ f405rg/Core/Inc/QuadEnc.h
@@ -9,25 +9,27 @@
 #define QUADENC_H_
 
 #include <Thread.h>
-#include "DigitalIn.h"
 
 #include "stm32f4xx_hal.h"
 
 class QuadEnc	: public Thread<QuadEnc> {
 public:
-	QuadEnc(DigitalIn ch1, DigitalIn ch2, const char *name, uint32_t stack_size = 512, osPriority_t priority = (osPriority_t)osPriorityNormal);
+	QuadEnc(const char *name, uint32_t stack_size = 512, osPriority_t priority = (osPriority_t)osPriorityNormal);
 	virtual ~QuadEnc();
 
+	uint16_t getCount();
+	void setCount();
+	void resetCount();
+	uint16_t getSpeed();
 	void run(void *argument);
 
 protected:
 	ThreadArg<QuadEnc> *threadArgs;
 
 private:
-	DigitalIn ch1;
-	DigitalIn ch2;
 	uint16_t currentPos;
 	uint16_t previousPos;
+	uint16_t positions[2][10];
 };
 
 #endif /* QUADENC_H_ */
--- f405rg/Core/Src/QuadEnc.cpp
+++ f405rg/Core/Src/QuadEnc.cpp
@@ -8,28 +8,44 @@
 
 #include "QuadEnc.h"
 
-QuadEnc::QuadEnc(DigitalIn ch1, DigitalIn ch2) {
+QuadEnc::QuadEnc() {
+  HAL_TIM_IC_Start_IT(&htim8, TIM_CHANNEL_1);   // main channel
+  HAL_TIM_IC_Start(&htim8, TIM_CHANNEL_2);   // indirect channel
+
+  HAL_TIM_IC_Start_IT(&htim12, TIM_CHANNEL_1);   // main channel
+  HAL_TIM_IC_Start(&htim12, TIM_CHANNEL_2);   // indirect channel
 
 	this->ch1 = ch1;
 	this->ch2 = ch2;
   this->currentPos = 0;
   this->previousPos = 0;
+  memset(this->positions, 0);
 }
 
 QuadEnc::~QuadEnc() {
 
 }
 
-uint16_t QuadEnc::getPos() {
+uint16_t QuadEnc::getCount() {
 	return
 }
 
-void QuadEnc::resetPos(){
+void QuadEnc::resetCount(){
   this->currentPos = 0;
   this->previousPos = 0;
 }
 
-void QuadEnc::incrementPos(){
-  this->currentPos = 0;
-  this->previousPos = 0;
+void QuadEnc::setCount(uint16_t count){
+  this->currentPos = count;
+  this->previousPos = count;
+}
+
+uint16_t QuadEnc::getSpeed(){
+  uint16_t speed = 0;
+  for(int i = 0; i < 10; i++){
+    speed += positions[0][i];
+  }
+
+  speed = speed / (positions[1][0] + positions[1][10])
+  return speed;
 }
