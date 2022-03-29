0195612f617c5a2dc036a513e8420a047eb8535b
Tyler Tucker <tylermatthewtucker@gmail.com>
2022-03-28 19:54:54 -0500

    Fixed header file for encoder class

--- f405rg/Core/Inc/QuadEnc.h
+++ f405rg/Core/Inc/QuadEnc.h
@@ -14,7 +14,7 @@
 
 #include "stm32f4xx_hal.h"
 
-class QuadEnc	: public QuadEnc {
+class QuadEnc{
 public:
 	QuadEnc(const char *name, uint32_t stack_size = 512, osPriority_t priority = (osPriority_t)osPriorityNormal);
 	virtual ~QuadEnc();
