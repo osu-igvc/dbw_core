7a81b64dee172b340728f511e7b88029a3ea485b
Tyler Tucker <tylermatthewtucker@gmail.com>
2022-03-29 15:23:36 -0500

    Fixed out of bounds array

--- f405rg/Core/Src/QuadEnc.cpp
+++ f405rg/Core/Src/QuadEnc.cpp
@@ -70,6 +70,6 @@ uint16_t QuadEnc::getSpeed(){
         speed += positions[0][i];
     }
 
-    speed = speed / (positions[1][0] + positions[1][10]);
+    speed = speed / (positions[1][0] + positions[1][9]);
     return speed;
 }
